25ed7b2248c7d82cae03e057ad0992f6b6b2936b
AlfredPolanco <39679348+AlfredPolanco@users.noreply.github.com>
2020-06-22 14:41:31 -0400

    Update AnagramFinder.cs

--- KataAnagram/AnagramFinder.cs
+++ KataAnagram/AnagramFinder.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 
 namespace KataAnagram
@@ -10,21 +11,18 @@ namespace KataAnagram
     {
         public List<string> FileReader(string p) 
         {
-			List<string> anagramsGroup = new List<string>(null);
+			List<string> anagramsGroup = new List<string>();
 			try
 			{
 				string[] lines = File.ReadAllLines(p, Encoding.UTF8);
 				var MyArrayLower = lines.Select(s => s.ToLowerInvariant()).ToArray();
 				anagramsGroup = new List<string>(MyArrayLower);
-
 			}
 			catch (Exception e )
 			{
-
 				Console.WriteLine("Please make sure the file is on txt format and it is on the specified route");
 			}
 			return anagramsGroup;
-
 		}
     }
 }
