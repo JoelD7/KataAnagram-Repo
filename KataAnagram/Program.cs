b6c17febd7b1a0eb92efb390084b6440bd40219e
Joel Fabián <joeld200013@gmail.com>
2020-06-22 10:41:21 -0400

    Creacion AnagramFinder

---
+++ KataAnagram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KataAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> listOfList = new List<List<string>>();
+            listOfList.Add(new string[]{"one","two"}.ToList());
+            listOfList.Add(new string[]{"3","4"}.ToList());
+            listOfList.Add(new string[]{"5","6"}.ToList());
+
+            listOfList.ForEach((list)=>{
+                System.Console.Write("List: ");
+                foreach (var item in list)
+                {
+                    System.Console.Write("{0}, ", item);
+                }
+                System.Console.WriteLine();
+            });
+        }
+    }
+}
