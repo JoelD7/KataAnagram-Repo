ff2fea8c2744fcecbc48fa43cb144089b4e1da36
Joel Fabián <joeld200013@gmail.com>
2020-06-22 20:14:54 -0400

    AnagramFinder Completado

---
+++ KataAnagram/AnagramFinder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+public class AnagramFinder
+{
+
+    public List<string> words;
+    public List<List<string>> anagramGroups;
+
+    public AnagramFinder(string filename)
+    {
+        words = FileReader.GetWords();
+        anagramGroups = new List<List<string>>();
+        IdentifyAnagrams();
+    }
+
+    private void IdentifyAnagrams()
+    {
+        foreach (var curWord in words)
+        {
+            List<string> anagrams = new List<string>();
+            foreach (var item in words)
+            {
+                if (IsAnagram(curWord, item))
+                {
+                    if (!IsIncluded(item))
+                    {
+                        anagrams.Add(item);
+                    }
+                }
+
+            }
+            if (anagrams.LongCount() > 1)
+            {
+                anagramGroups.Add(anagrams);
+            }
+        }
+    }
+
+    private bool IsIncluded(string word)
+    {
+        bool included = false;
+        anagramGroups.ForEach(list =>
+        {
+            if (list.Contains(word))
+            {
+                included = true;
+            }
+        });
+        return included;
+    }
+
+    private bool IsAnagram(string word, string word2)
+    {
+        if (word.Length != word2.Length)
+        {
+            return false;
+        }
+
+        int size = word.Length;
+        int acum = 0;
+
+        List<char> list = word.ToCharArray().ToList();
+        for (int i = 0; i < list.LongCount(); i++)
+        {
+            char ch = list.ElementAt(i);
+            if (HasChar(word2, ch))
+            {
+                acum++;
+            }
+        }
+        return acum == size;
+    }
+
+    private bool HasChar(string s, char c)
+    {
+        bool has = false;
+        s.ToCharArray().ToList().ForEach(ch =>
+        {
+            if (ch == c)
+            {
+                has = true;
+            }
+        });
+        return has;
+    }
+
+    public List<List<string>> GetAnagramGroups()
+    {
+        return anagramGroups;
+    }
+
+
+}
---
+++ KataAnagram/TestAnagramFinder.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+public class TestAnagramFinder
+{
+    [Fact]
+
+    public void test()
+    {
+        List<List<string>> expected = new List<List<string>>();
+        expected.Add(new string[] { "tar", "rat" }.ToList());
+        expected.Add(new string[] { "arc", "car" }.ToList());
+        expected.Add(new string[] { "glean", "angel" }.ToList());
+        expected.Add(new string[] { "stressed", "desserts" }.ToList());
+        expected.Add(new string[] { "state", "taste" }.ToList());
+        expected.Add(new string[] { "elbow", "below" }.ToList());
+        expected.Add(new string[] { "cried", "cider" }.ToList());
+        expected.Add(new string[] { "dusty", "study" }.ToList());
+        expected.Add(new string[] { "pairs", "paris" }.ToList());
+        expected.Add(new string[] { "chin", "inch" }.ToList());
+        expected.Add(new string[] { "night", "thing" }.ToList());
+        expected.Add(new string[] { "robed", "bored" }.ToList());
+        expected.Add(new string[] { "grab", "brag" }.ToList());
+        expected.Add(new string[] { "cat", "act" }.ToList());
+        expected.Add(new string[] { "nerved", "denver" }.ToList());
+        expected.Add(new string[] { "save", "vase" }.ToList());
+        expected.Add(new string[] { "votes", "stove" }.ToList());
+        expected.Add(new string[] { "fried", "fired" }.ToList());
+        expected.Add(new string[] { "untied", "united" }.ToList());
+        expected.Add(new string[] { "dog", "god" }.ToList());
+        expected.Add(new string[] { "wolf", "flow" }.ToList());
+        expected.Add(new string[] { "hear", "here" }.ToList());
+        expected.Add(new string[] { "teacher", "cheater" }.ToList());
+        expected.Add(new string[] { "break", "baker" }.ToList());
+        expected.Add(new string[] { "rebuild", "builder" }.ToList());
+        expected.Add(new string[] { "dice", "iced" }.ToList());
+        expected.Add(new string[] { "senator", "treason" }.ToList());
+        expected.Add(new string[] { "kitchen", "thicken" }.ToList());
+        AnagramFinder anagram = new AnagramFinder("testanagram.txt");
+        List<List<string>> actual = anagram.GetAnagramGroups();
+
+        Assert.Equal(expected, actual);
+
+    }
+
+
+
+}
